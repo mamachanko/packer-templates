c700abc8ba69d15e87429da350141a03f32764ca
mamachanko <sugardubz@gmail.com>
2016-03-31 09:31:04 +0200

    integrate ansible

---
+++ scripts/ubuntu/ansible.sh
@@ -0,0 +1,9 @@
+#! /bin/bash -eux
+
+# Install Ansible repository.
+sudo apt-get -y install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+
+# Install Ansible.
+sudo apt-get -y update
+sudo apt-get -y install ansible
